a7783397586db92ad520e7c5e38ebb8c1149aa70
曲洪澎 <hamsiny@quhongpengdeMacBook-Pro.local>
2021-07-12 15:01:12 +1200

    add header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <nav className="navbar navbar-dark px-4">
+                <span className="navbar-brand mb-0 py-2 px-3"><h2>Vanila JS Demo</h2></span>
+            </nav>
+        </div>
+    )
+}
+
+export default Header
