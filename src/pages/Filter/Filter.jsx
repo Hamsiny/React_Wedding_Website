10d956f018b4436e4933ce48cf52651b48ecb5a2
Hamsiny <459516309@qq.com>
2021-07-14 11:54:06 +1200

    change filter comp from function to class

--- src/pages/Filter/Filter.jsx
+++ src/pages/Filter/Filter.jsx
@@ -1,40 +1,30 @@
-import React, { useState, useEffect } from 'react'
+import React, { Component } from 'react'
 import rawdata from '../../data/data.js'
 import ShowBox from '../ShowBox/ShowBox'
 import './Filter.css'
 
-const Filter = () => {  
-    const [data, setData] = useState([]);
-
-    useEffect(() => {
-        const getData = () => {
-            setData(rawdata); 
-        }
-        getData(); 
-    }, []);
+export class Filter extends Component {  
+    state = {
+        data: rawdata
+    };
 
     // componentdidmount
 
-    // state = {
-    //     data: []
-    // }
-
     // whole filter function
-    const showWithFilter = () => {
-        // setData(rawdata);
-        let proWithCate = filterCategory(rawdata, document.getElementById('category').value);
-        let proWithFilter = filterPrice(proWithCate, document.getElementById('price').value);
-        setData(proWithFilter);
-        // console.log(data);
+    showWithFilter = () => {
+        this.setState({data: rawdata});
+        const proWithCate = this.filterCategory(rawdata, document.getElementById('category').value);
+        const proWithFilter = this.filterPrice(proWithCate, document.getElementById('price').value);
+        this.setState({data: proWithFilter});
     }
     
     // category filter
-    const filterCategory = (productsArray, cateId) => {
+    filterCategory = (productsArray, cateId) => {
         return productsArray.filter(product => (product.categoryId == cateId || cateId == "0"));
     }
 
     // price filter
-    const filterPrice = (productsArray, priceFilter) => {
+    filterPrice = (productsArray, priceFilter) => {
         let priceProducts;
         switch (priceFilter) {
             case "0":
@@ -63,89 +53,87 @@ const Filter = () => {
     }
 
     // sorting products ascending
-    const productsAsc = () => {
-        console.log(data);
-        const tempData = data.filter(prod => prod.prodId != 90);
-        setData(tempData.sort((a, b) => a.price - b.price));
-        console.log(data);
+    productsAsc = () => {
+        this.setState({data: this.state.data.sort((a, b) => a.price - b.price)});
     }
 
     // sorting products descending
-    const productsDesc = () => {
-        console.log(data);
-        setData(data.sort((a, b) => b.price - a.price));
-        console.log(data);
+    productsDesc = () => {
+        this.setState({data: this.state.data.sort((a, b) => b.price - a.price)});
     }
 
     // reset products to origin
-    const resetFilter = () => {
+    resetFilter = () => {
         document.getElementById('category').value = "0";
         document.getElementById('price').value = "0";
-        setData(rawdata);
+        this.setState({data: rawdata});
     }
     
-    return (
-        <>
-            <div className="row mt-3 mx-4">
-                <div className="col-lg-3 col-md-6 col-sm-12 mt-3">
-                    <h4>Category</h4>
-                    <select id="category" className="categoryselector">
-                        <option value="0">All Hires</option>
-                        <option value="1">ARCHES & BALLOON/FLOWER STANDS</option>
-                        <option value="2">CARTS</option>
-                        <option value="3">PARTY HIRE</option>
-                        <option value="5">TABLEWARE</option>
-                        <option value="25">LED LETTER & NUMBER LIGHTS</option>
-                        <option value="11">CENTREPIECE HIRE - CANDLES & VASES</option>
-                        <option value="15">BACKDROPS & FLOWER WALLS</option>
-                        <option value="21">CAKE & DESSERT STANDS</option>
-                        <option value="18">TABLES & CONSOLES</option>
-                        <option value="12">PLINTHS</option>
-                        <option value="27">TABLECLOTHS</option>
-                        <option value="16">CHAIRS</option>
-                        <option value="20">WOODEN CRATES</option>
-                        <option value="51">MIRRORS</option>
-                        <option value="14">WEDDING HIRE</option>
-                        <option value="10">BABY SHOWER</option>
-                        <option value="47">Luxe Dream Wedding Flowers</option>
-                        <option value="49">Luxe Dream Floral Studio</option>
-                    </select>
-                    <button className="btnn mx-1" onClick={showWithFilter}>
-                        Show
-                    </button>
+    render() {
+        return (
+            <>
+                <div className="row mt-3 mx-4">
+                    <div className="col-lg-3 col-md-6 col-sm-12 mt-3">
+                        <h4>Category</h4>
+                        <select id="category" className="categoryselector">
+                            <option value="0">All Hires</option>
+                            <option value="1">ARCHES & BALLOON/FLOWER STANDS</option>
+                            <option value="2">CARTS</option>
+                            <option value="3">PARTY HIRE</option>
+                            <option value="5">TABLEWARE</option>
+                            <option value="25">LED LETTER & NUMBER LIGHTS</option>
+                            <option value="11">CENTREPIECE HIRE - CANDLES & VASES</option>
+                            <option value="15">BACKDROPS & FLOWER WALLS</option>
+                            <option value="21">CAKE & DESSERT STANDS</option>
+                            <option value="18">TABLES & CONSOLES</option>
+                            <option value="12">PLINTHS</option>
+                            <option value="27">TABLECLOTHS</option>
+                            <option value="16">CHAIRS</option>
+                            <option value="20">WOODEN CRATES</option>
+                            <option value="51">MIRRORS</option>
+                            <option value="14">WEDDING HIRE</option>
+                            <option value="10">BABY SHOWER</option>
+                            <option value="47">Luxe Dream Wedding Flowers</option>
+                            <option value="49">Luxe Dream Floral Studio</option>
+                        </select>
+                        <button className="btnn mx-1" onClick={this.showWithFilter}>
+                            Show
+                        </button>
+                    </div>
+                    <div className="col-lg-3 col-md-6 col-sm-12 mt-3">
+                        <h4>Price</h4>
+                        <select id="price" className="priceselector">
+                            <option value="0">All</option>
+                            <option value="100">0 - 100</option>
+                            <option value="500">101 - 500</option>
+                            <option value="1000">501 - 1000</option>
+                            <option value="1001">Over 1000</option>
+                        </select>
+                        <button className="btnn mx-1" onClick={this.showWithFilter}>
+                            Filter
+                        </button>
+                    </div>
+                    <div className="col-lg-3 col-md-6 col-sm-12 mt-3">
+                        <h4>Sort</h4>
+                        <button className="btnn mr-2" onClick={this.productsAsc}>
+                            Ascending
+                        </button>
+                        <button className="btnn mr-2" onClick={this.productsDesc}>
+                            Decending
+                        </button>
+                    </div>
+                    <div className="col-lg-3 col-md-6 col-sm-12 mt-3">
+                        <h4>Reset</h4>
+                        <button className="btnn mr-2" onClick={this.resetFilter}>
+                            Reset
+                        </button>
+                    </div>
                 </div>
-                <div className="col-lg-3 col-md-6 col-sm-12 mt-3">
-                    <h4>Price</h4>
-                    <select id="price" className="priceselector">
-                        <option value="0">All</option>
-                        <option value="100">0 - 100</option>
-                        <option value="500">101 - 500</option>
-                        <option value="1000">501 - 1000</option>
-                        <option value="1001">Over 1000</option>
-                    </select>
-                    <button className="btnn mx-1" onClick={showWithFilter}>
-                        Filter
-                    </button>
-                </div>
-                <div className="col-lg-3 col-md-6 col-sm-12 mt-3">
-                    <h4>Sort</h4>
-                    <button className="btnn mr-2" onClick={productsAsc}>
-                        Ascending
-                    </button>
-                    <button className="btnn mr-2" onClick={productsDesc}>
-                        Decending
-                    </button>
-                </div>
-                <div className="col-lg-3 col-md-6 col-sm-12 mt-3">
-                    <h4>Reset</h4>
-                    <button className="btnn mr-2" onClick={resetFilter}>
-                        Reset
-                    </button>
-                </div>
-            </div>
-            <ShowBox products={data}/>
-        </>
-    )
+                <ShowBox products={this.state.data}/>
+            </>
+        );
+    }
+    
 }
 
 export default Filter
